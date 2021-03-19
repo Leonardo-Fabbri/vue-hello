091de0d0566706d2998f5531c81cad0ba858e587
Leonardo <leonardofabbri125@gmail.com>
2021-03-19 10:10:39 +0100

    kayak!!!!!

--- javascript.js
+++ javascript.js
@@ -2,8 +2,9 @@ var root = new Vue(
     {
         el: '#root',
         data: {
-            message:"SONO UN TITOLO INTERESSANTE "
-        } 
+            message:"MI MANCA ANDARE IN FIUME ",
+            img:"https://lh3.googleusercontent.com/proxy/GPhZxhMzJX7BGm3OsKshVpkKrG3vQkUFvnuWI-rsRJePJnUsXS8IDa_PAronRZ0kZVLDFNIyfn3I83UzkuJEe1DzUlTLpY8tdddGmlr7jUr__LQkAZbxfBdeZpqMcF8wRIAbPcAnzA",
+        }
     }
 );
 
